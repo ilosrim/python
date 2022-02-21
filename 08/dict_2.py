2cc79ae50bfc499d7bef1b000e8a8c7e77f24c76
ilosrim <ilosrim@yahoo.com>
2022-02-21 23:27:03 +0500

    08

--- 08/dict_2.py
+++ 08/dict_2.py
@@ -13,6 +13,8 @@ for key, value in talaba_0.items():
 
 telefonlar = {
     'ali':'iphone x',
+    'vali':'iphone x',
+    'halim':'iphone x',
     'vali':'galaxy s9',
     'olim':'mi 10 pro',
     'orif':'nokia 3310',
@@ -30,7 +32,9 @@ mahsulotlar = { # Do'kondagi mahsulotlar
   'shaftoli':30000
     }
 
+# keys()
 print(mahsulotlar.keys())
+
 print('Do\'kondagi mahsulotlar:')
 for mahsulot in mahsulotlar.keys():
   print(mahsulot.title())
@@ -39,3 +43,59 @@ bozorlik = ['anor','uzum','non','baliq']
 for mahsulot in mahsulotlar:
   if mahsulot in bozorlik:
     print(f"{mahsulot.title()} {mahsulotlar[mahsulot]} so'm")
+
+print("Do'konimizdagi mahsulotlar:")
+for mahsulot in sorted(mahsulotlar):
+  print(mahsulot.title())
+
+# values()
+print(telefonlar.values())
+for tel in set(telefonlar.values()):
+  print(tel.title())
+
+p_dict = {
+  "variables":"o'zgaruvchilar, ular orqali har xil malumotlarni saqlab olsak bo'ladi",
+  "string":"matn malumot turi.",
+  "integer":"son malumot turi.",
+  "boolean":"mantiqiy qiymat malumot turi.",
+  "list":"har xil turdagi malumotlarni o'zida saqlovchi malumot turi.",
+  "tuoles":"o'zgarmas list yaratish",
+  "for":"list yoki shu kabi malumotlar ichidan aylanib o'tish uchun hizmat qiluvchi funksiya.",
+  "while":"list yoki shu kabi malumotlar ichidan aylanib o'tish uchun hizmat qiluvchi funksiya.",
+  "dictionary":"key-value pairs ko'rinishida malumot saqlovchi tur."
+}
+for k, v in sorted(p_dict.items()):
+  print(f"{k} so'zining manosi - {v}.")
+
+davlatlar = {
+  "usa":"washington",
+  "uzbbekistan":"tashkent",
+  "great britain":"london",
+  "germany":"berlin",
+  "italy":"rome",
+  "france":"paris",
+  "chine":"pekin",
+  "japan":"tokio"
+}
+
+print("Davlat nomi:\t Davlat poytaxti:")
+for k, v in sorted(davlatlar.items()):
+  print(f"{k.title()}\t\t {v.title()}")
+
+# print('Dunyo davlatlari:')
+# for davlat in sorted(davlatlar):
+#     print(davlat.upper())
+
+# print('\nDavlatlarning poytaxtlari')
+# for poytaxt in sorted(davlatlar.values()):
+#   print(poytaxt.title())
+
+davlat = input("Qaysi davlat poytaxtini bilishni hohlaysiz?\n").lower()
+# for k, v in davlatlar.items():
+  if davlat in davlatlar.keys():
+    if davlat == 'usa':
+      print(f"{davlat.upper()}-ning poytaxti {v.title()}.")
+    else:
+      print(f"{davlat.title()}-ning poytaxti {v.title()}.")
+  else:
+    print("Bu davlat haqida ma'lumot hozircha yo'q.")
