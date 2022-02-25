fc0d34c7704b7a2f56e7dcb934f935bfea6ee953
ilosrim <ilosrim@yahoo.com>
2022-02-25 20:27:59 +0500

    while

---
+++ 09/while.py
@@ -0,0 +1,10 @@
+# Input
+# ism = input("Ismingizni kiriting: ")
+# yosh = int(input(f"Salom {ism.title()}, yoshingiz nechchida: "))
+# print(f"{ism.title()}, siz {yosh} yoshdasiz.")
+
+# while
+son = 1
+while son<5:
+  print(son, end=' ')
+  son = son + 1
